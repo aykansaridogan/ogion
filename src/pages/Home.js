6b52673937c5bf904a4aebaa1e54a8a64513e865
aykansaridogan <aykansaridogan@hotmail.com>
2024-06-12 14:15:14 +0300

    reklam2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,12 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Sliders from "../utils/Slider";
-import "./styles.css"; // CSS dosyasını içe aktarın
 
 const Home = () => {
-  return (
-    <div>
-      <div className="ad-container">
-        <div className="ad ad-left">
-          {/* Google AdSense sol üst reklam kodu */}
-          <ins className="adsbygoogle"
-            style={{ display: "block" }}
-            data-ad-client="ca-pub-xxxxxxxxxx"
-            data-ad-slot="xxxxxxxxxx"
-            data-ad-format="auto"></ins>
-          <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-          </script>
-        </div>
-        <div className="ad ad-right">
-          {/* Google AdSense sağ üst reklam kodu */}
-          <ins className="adsbygoogle"
-            style={{ display: "block" }}
-            data-ad-client="ca-pub-xxxxxxxxxx"
-            data-ad-slot="xxxxxxxxxx"
-            data-ad-format="auto"></ins>
-          <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-          </script>
-        </div>
-      </div>
-      <Sliders />
+ 
+
+  return (<div>
+    <Sliders/>
     </div>
   );
 };
